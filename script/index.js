34e5cc35c081fe2314e35c49ac5f160ece840964
MdRaihan3 <1mdr91012@gmail.com>
2024-02-18 23:09:30 +0600

    added file

---
+++ script/index.js
@@ -0,0 +1,40 @@
+
+const allSeats = document.getElementsByClassName('select-seat');
+let totalSelectedSeatCount = 0;
+let totalSeatLeft = getElementNumValueById('total-seat-left');
+let totalPrice = 0;
+let grandTotalPrice = 0;
+
+for (const seat of allSeats) {
+    seat.addEventListener("click", function (event) {
+        totalSelectedSeatCount = totalSelectedSeatCount + 1;
+        totalSeatLeft = totalSeatLeft - 1;
+        totalPrice = 550 + totalPrice;
+        if (totalSelectedSeatCount <= 4) {                  
+                    setElementTextById('selected-seat-numbers', totalSelectedSeatCount);
+                    event.target.classList.add('bg-[#1DD100]');
+                    setElementTextById('total-seat-left', totalSeatLeft);
+                    createSelectedSeatDiv(event);
+                    setElementTextById('total-price', totalPrice);           
+        }
+        else {
+            return;
+        }
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ script/utility.js
@@ -0,0 +1,36 @@
+function getElementNumValueById(elementId){
+const elementText = document.getElementById(elementId).innerText;
+const elementNumValue = parseInt(elementText)
+return elementNumValue;
+}
+
+function setElementTextById(elementId,value){
+    const element = document.getElementById(elementId)
+    element.innerText = value;
+    }
+
+function createSelectedSeatDiv(event){
+const getDiv = document.getElementById('create-selected-seat-div')
+
+const newDiv = document.createElement('div')
+newDiv.classList.add('new-div');
+getDiv.appendChild(newDiv);
+
+const targetSeatText = event.target.innerText;
+const newH3 = document.createElement('h3')
+newH3.innerText = targetSeatText;
+newDiv.appendChild(newH3)
+
+const newH4 = document.createElement('h4')
+newH4.innerText = 'Economy';
+newDiv.appendChild(newH4)
+
+const newP = document.createElement('p')
+newP.innerText = '550';
+newDiv.appendChild(newP)
+    }
+
+
+
+
+
