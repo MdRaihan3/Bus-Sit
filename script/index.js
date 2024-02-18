1aee0b88ace26329d4ae992197084a71382df4ff
MdRaihan3 <1mdr91012@gmail.com>
2024-02-18 23:14:04 +0600

    added total price

--- script/index.js
+++ script/index.js
@@ -10,12 +10,14 @@ for (const seat of allSeats) {
         totalSelectedSeatCount = totalSelectedSeatCount + 1;
         totalSeatLeft = totalSeatLeft - 1;
         totalPrice = 550 + totalPrice;
+        grandTotalPrice = totalPrice;
         if (totalSelectedSeatCount <= 4) {                  
                     setElementTextById('selected-seat-numbers', totalSelectedSeatCount);
                     event.target.classList.add('bg-[#1DD100]');
                     setElementTextById('total-seat-left', totalSeatLeft);
                     createSelectedSeatDiv(event);
                     setElementTextById('total-price', totalPrice);           
+                    setElementTextById('grand-total-price',grandTotalPrice)
         }
         else {
             return;
