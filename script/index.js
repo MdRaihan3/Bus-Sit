618a00a5d963bd386318622f2fd5b58cfc3c94e5
MdRaihan3 <1mdr91012@gmail.com>
2024-02-19 11:27:14 +0600

    done responsive

--- script/index.js
+++ script/index.js
@@ -11,13 +11,41 @@ for (const seat of allSeats) {
         totalSeatLeft = totalSeatLeft - 1;
         totalPrice = 550 + totalPrice;
         grandTotalPrice = totalPrice;
-        if (totalSelectedSeatCount <= 4) {                  
-                    setElementTextById('selected-seat-numbers', totalSelectedSeatCount);
-                    event.target.classList.add('bg-[#1DD100]');
-                    setElementTextById('total-seat-left', totalSeatLeft);
-                    createSelectedSeatDiv(event);
-                    setElementTextById('total-price', totalPrice);           
-                    setElementTextById('grand-total-price',grandTotalPrice)
+        const couponText = document.getElementById('coupon-text').value;
+
+                if (totalSelectedSeatCount < 5) {     
+                    
+            if((couponText=='NEW15') && (totalSelectedSeatCount == 4)){
+                const applyButton = document.getElementById('apply-button');
+                applyButton.classList.remove('btn-disabled');
+                setElementTextById('selected-seat-numbers', totalSelectedSeatCount);
+                event.target.classList.add('bg-[#1DD100]','text-white');
+                
+                setElementTextById('total-seat-left', totalSeatLeft);
+                createSelectedSeatDiv(event);
+                setElementTextById('total-price', totalPrice);                
+                grandTotalPrice = totalPrice - totalPrice*15/100;
+                setElementTextById('grand-total-price',grandTotalPrice)
+            }   
+            else if((totalSelectedSeatCount == 4) && (couponText =='Couple 20')){
+                const applyButton = document.getElementById('apply-button');
+                applyButton.classList.remove('btn-disabled');
+                setElementTextById('selected-seat-numbers', totalSelectedSeatCount);
+                event.target.classList.add('bg-[#1DD100]','text-white');
+                setElementTextById('total-seat-left', totalSeatLeft);
+                createSelectedSeatDiv(event);
+                setElementTextById('total-price', totalPrice); 
+                grandTotalPrice = totalPrice - totalPrice*20/100;
+                setElementTextById('grand-total-price',grandTotalPrice)
+            } 
+            else{
+                setElementTextById('selected-seat-numbers', totalSelectedSeatCount);
+                event.target.classList.add('bg-[#1DD100]','text-white');
+                setElementTextById('total-seat-left', totalSeatLeft);
+                createSelectedSeatDiv(event);
+                setElementTextById('total-price', totalPrice);           
+                setElementTextById('grand-total-price',grandTotalPrice) 
+            }                                         
         }
         else {
             return;
